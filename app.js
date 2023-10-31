983d0e2b58dfd2e23004b2641623b98dba85861b
mszahan <mszahan1@gmail.com>
2023-10-31 22:44:37 +0600

    created shorthand for eventlisteners and queryselectors

---
+++ app.js
@@ -0,0 +1,19 @@
+// it's better to wait for the event for manipulation
+window.addEventListener('DOMContentLoaded', ()=>{
+    let nav = document.querySelector('nav');
+    console.log(nav)
+
+})
+
+
+// // you can create shorthand to use something big
+// //......over and over again
+// const $ = function(args){ return document.querySelector(args);}
+// const $$ = function(args){ return document.querySelectorAll(args);}
+// // now you don't have to type the whole function like document.ge..blah blah
+// //.. just use $ sign it will work as well
+
+// HTMLElement.prototype.on = function(a, b, c){ return this.addEventListener(a, b, c); }
+// HTMLElement.prototype.off = function(a, b){ return this.removeEventListener(a, b); }
+// HTMLElement.prototype.$ = function(s){ return this.querySelector(s); }
+// HTMLElement.prototype.$$ = function(s){ return this.querySelectorAll(s); }
