ecc333e9a04235a3bb6deb823989de4294466607
mszahan <mszahan1@gmail.com>
2023-11-04 19:26:42 +0600

    created shadow dom and attached root children to it

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -1,13 +1,15 @@
 export class MenuPage extends HTMLElement {
     constructor(){
         super();
+
+        this.root = this.attachShadow({mode: 'open'});
         
     }
     // when the component is attached to the DOM
     connectedCallback(){
         const template = document.getElementById('menu-page-template');
         const content = template.content.cloneNode(true);
-        this.appendChild(content);
+        this.root.appendChild(content);
 
     }
 }
