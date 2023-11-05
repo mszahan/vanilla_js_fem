4ced941f114d1e0f17c1651ba60beeb346f73f86
mszahan <mszahan1@gmail.com>
2023-11-05 10:39:21 +0600

    added proxy to the store

--- services/Store.js
+++ services/Store.js
@@ -3,4 +3,17 @@ const Store = {
     cart: []
 }
 
-export default Store;
+const proxiedStore = new Proxy(Store, {
+    set(target, property, value){
+        target[property] = value;
+        if (property=='menu'){
+            window.dispatchEvent(new Event('appmenuchange'))
+        }
+        if (property=='cart'){
+            window.dispatchEvent(new Event('appcartchange'))
+        }
+    }
+
+})
+
+export default proxiedStore;
